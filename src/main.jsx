83ce54963a3e1c563ab255436a996cf7fe1dfe2f
omarjafor <omarjaforchy@gmail.com>
2023-11-02 22:35:51 +0600

    just practicin auth provier

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Root from './Root/Root';
 import Home from './Components/Home/Home';
+import AuthProvider from './Provider/AuthProvider';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
