19599bdcec09abee2336a6ca54d7a57e2b82c3da
omarjafor <omarjaforchy@gmail.com>
2023-11-02 20:28:16 +0600

    just practicing some work

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 
 function Navbar() {
   const [mode, setMode] = useState('light');
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Navbar from './Components/Navbar/Navbar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <Navbar></Navbar>
   </React.StrictMode>,
 )
