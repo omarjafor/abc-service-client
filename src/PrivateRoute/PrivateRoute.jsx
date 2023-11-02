0d58d79d55888baa65ad5ebe324cef51c2cb6ff3
omarjafor <omarjaforchy@gmail.com>
2023-11-03 01:14:54 +0600

    just practicin private route

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -1,11 +1,17 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../Hooks/useAuth";
 
+const PrivateRoute = ({children}) => {
+    const location = useLocation();
+    const { user, loading } = useAuth();
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    if (loading) return <span className="loading loading-spinner loading-lg"></span> ;
+
+    if(!user?.email){
+        return <Navigate state={location} to='/login'></Navigate>
+    }
+
+    return children;
 };
 
 export default PrivateRoute;
