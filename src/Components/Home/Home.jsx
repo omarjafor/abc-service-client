98971d2f1774b208b62e6f503d337956fc0e7354
omarjafor <omarjaforchy@gmail.com>
2023-11-03 12:33:33 +0600

    some modifying with practice

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "./Banner/Banner";
 
 
 const Home = () => {
     return (
-        <div className="text-black dark:text-white text-7xl h-[120vh]">
-            This is Home
+        <div className="">
+            <Banner></Banner>
         </div>
     );
 };
