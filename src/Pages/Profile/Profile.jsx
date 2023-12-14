66af5d2858a91e2090ed77d89a3afd48c91607a6
omarjafor <omarjaforchy@gmail.com>
2023-12-14 22:59:52 +0600

    some update and issue fixed

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import { AiFillFacebook, AiFillTwitterSquare, AiOutlineInstagram } from 'react-icons/ai';
-
+import useAuth from "../../Hooks/useAuth";
 
 const Profile = () => {
     const { user } = useAuth();
