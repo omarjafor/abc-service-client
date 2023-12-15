463ef2fa9782f5f7552452c8b4349c01c9cb0d32
omarjafor <omarjaforchy@gmail.com>
2023-12-15 11:25:44 +0600

    some update and issue fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Link, NavLink } from "react-router-dom";
-
+import useAuth from '../../Hooks/useAuth';
+import logo from './../../../public/logo.png'
 
 function Navbar() {
   const [mode, setMode] = useState('light');
